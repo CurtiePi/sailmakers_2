83316a12ee04711cd4f0660bd24684ecf750f795
CurtiePi <curtisbklyn@gmail.com>
2022-04-05 11:15:58 -0400

    Added the server controller functions

---
+++ controllers/dataAccessController.js
@@ -0,0 +1,350 @@
+const mongoose          = require('mongoose');
+const Customer          = require('../models/customers');
+const Salesperson       = require('../models/salespeople');
+const Quote             = require('../models/quotes');
+const Port              = require('../models/ports');
+
+
+module.exports = {
+    /**
+     * List all customers in the database
+     *
+     */
+    listCustomers: async () => {
+        try {
+            let customers = await Customer.find()
+                                          .populate({
+                                              path: 'quotes',
+                                              populate: [{
+                                                path: 'customer',
+                                                model: 'Customer'
+                                              },
+                                              { path: 'salesperson',
+                                                model: 'Salesperson'
+                                              }]
+                                           });
+
+            return customers;
+        }
+        catch (err) {
+            console.log('Received an error getting customer listing');
+        }
+    },
+
+    /**
+     * Get specific customers by id
+     *
+     */
+    getCustomerById: async (id) => {
+        try {
+            var user_id = mongoose.Types.ObjectId(id);
+            let customer = await Customer.findById({_id: user_id});
+            return customer;
+        }
+        catch (err) {
+            console.log('Received an error getting customer');
+        }
+    },
+    createCustomer: async (customer_data) => {
+        try {
+            var customer = new Customer(customer_data);
+            await customer.save();
+
+            return customer;
+        }
+        catch(err) {
+            console.log('Received an error creating customer');
+        }
+    },
+
+    /**
+     * Update specific customer
+     *
+     */
+    updateCustomer: async (criteria, update ) => {
+        if (criteria._id) {
+            criteria._id = mongoose.Types.ObjectId(criteria._id);
+        }
+
+        try {
+            let customer = await Customer.findOneAndUpdate(criteria, update, {new: true});
+            return customer;
+        }
+        catch (err) {
+            console.log('Received an error updating customer');
+        }
+    },
+    /**
+     * Delete Customer
+     *
+     */
+    deleteCustomer: async (customer) => {
+        var c_id = mongoose.Types.ObjectId(customer._id);
+        var c_quotes = customer.quotes;
+        var quote_ids = [];
+        var sales_ids = [];
+        var sales_controller = [];
+        var pdf_list = [];
+        for (var idx=0; idx < c_quotes.length; idx++) {
+            var quote = c_quotes[idx];
+            quote_ids.push(mongoose.Types.ObjectId(quote._id));
+            pdf_list.push(...quote.doc_path);
+            if (! sales_controller.includes(quote.salesperson._id)) {
+                sales_controller.push(quote.salesperson._id);
+                sales_ids.push(mongoose.Types.ObjectId(quote.salesperson._id));
+            }
+        }
+
+        try {
+            // Need to remove quote from the and salesperson
+            if (quote_ids.length > 0) {
+              await Quote.deleteMany({_id: {$in: quote_ids}});
+              await Salesperson.updateMany({_id: {$in: sales_ids}}, {$pull: {quotes: {$in:quote_ids}}});
+            }
+            await Customer.deleteOne({_id: c_id});
+
+
+            return pdf_list;
+        }
+        catch(err) {
+            console.log('Received an error deleting customer');
+        }
+    },
+
+    listQuotes: async () => {
+        try {
+            let quotes = await Quote.find()
+                                       .sort({createAt: -1})
+                                       .populate('customer')
+                                       .populate('salesperson');
+            return quotes;
+        }
+        catch(err) {
+            console.log('Received an error getting quote listing');
+        }
+    },
+    getQuoteById: async (id) => {
+        try {
+            var quote_id = mongoose.Types.ObjectId(id);
+            let quote = await Quote.findById({_id: quote_id});
+            return quote;
+        }
+        catch(err) {
+            console.log('Received an error getting quote');
+        }
+    },
+    getCustomerQuotes: async (cid) => {
+        try {
+            var customer_id = mongoose.Types.ObjectId(cid);
+            let quotes = await Quote.find({customer: customer_id})
+                                    .sort({createAt: -1})
+                                    .populate('customer')
+                                    .populate('salesperson');
+            return quotes;
+        }
+        catch(err) {
+            console.log('Received an error getting quote');
+        }
+    },
+    createQuote: async (quote_data) => {
+        try {
+            // Make sure customer id and salesperson id are Object ids
+            customer_id = mongoose.Types.ObjectId(quote_data.customer_id);
+            salesper_id = mongoose.Types.ObjectId(quote_data.salesperson_id);
+
+            quote_data.customer = customer_id;
+            quote_data.salesperson = salesper_id;
+
+            delete quote_data['customer_id'];
+            delete quote_data['salesperson_id'];
+
+            var quote = new Quote(quote_data);
+            await quote.save();
+
+            // Need to populate the customer and salesperson with the quote
+            await Customer.findOneAndUpdate({_id: customer_id}, {$push: {quotes: quote._id}});
+            await Salesperson.findOneAndUpdate({_id: salesper_id}, {$push: {quotes: quote._id}});
+            
+            await Quote.populate(quote, { path: "customer" });
+            await Quote.populate(quote, { path: "salesperson" });
+
+            return quote;
+        }
+        catch(err) {
+            console.log('Received an error creating quote');
+        }
+    },
+    deleteQuote: async (quote) => {
+        var q_id = mongoose.Types.ObjectId(quote._id);
+        var c_id = mongoose.Types.ObjectId(quote.customer._id);
+        var s_id = mongoose.Types.ObjectId(quote.salesperson._id);
+
+        try {
+            var pdf_list = quote.doc_path;
+
+            // Need to remove quote from the customer and salesperson
+            await Customer.findOneAndUpdate({_id: c_id}, {$pull: {quotes: q_id}});
+            await Salesperson.findOneAndUpdate({_id: s_id}, {$pull: {quotes: q_id}});
+            
+            await Quote.deleteOne({_id: q_id});
+            return pdf_list
+        }
+        catch(err) {
+            console.log('Received an error deleting quote');
+        }
+    },
+    /**
+     * Update specific quote
+     *
+     */
+    updateQuote: async (criteria, update ) => {
+        if (criteria._id) {
+            criteria._id = mongoose.Types.ObjectId(criteria._id);
+        }
+
+        try {
+            let quote = await Quote.findOneAndUpdate(criteria, update, {new: true});
+            await Quote.populate(quote, { path: "customer" });
+            await Quote.populate(quote, { path: "salesperson" });
+            return quote;
+        }
+        catch (err) {
+            console.log('Received an error updating quote');
+        }
+    },
+    addQuoteDoc: async (quoteId, filePath ) => {
+        const criteria = { '_id': mongoose.Types.ObjectId(quoteId) };
+
+        try {
+            let quote = await Quote.findByIdAndUpdate(criteria, { $push: { doc_path: filePath} }, {new: true});
+            await Quote.populate(quote, { path: "customer" });
+            await Quote.populate(quote, { path: "salesperson" });
+            return quote;
+        }
+        catch (err) {
+            console.log('Received an error updating quote');
+        }
+    },
+
+    getSalespeople: async () => {
+        try {
+            let salespeople = await Salesperson.find();
+            return salespeople;
+        }
+        catch(err) {
+            console.log('Received an error getting listing of salespeople');
+        }
+    },
+    getSalespersonById: async (id) => {
+        try {
+            var salesperson_id = mongoose.Types.ObjectId(id);
+            let salesperson = await Salesperson.findById({_id: salesperson_id});
+            return salesperson;
+        }
+        catch(err) {
+            console.log('Received an error getting salesperson');
+        }
+    },
+    getSalespersonByName: async (name_param) => {
+        try {
+            var nameArr= name_param.split(' ');
+            let salesperson = await Salesperson.find({lname: nameArr[1], fname: nameArr[0]});
+            return salesperson;
+        }
+        catch(err) {
+            console.log('Received an error getting salesperson');
+        }
+    },
+    createSalesperson: async (salesperson_data) => {
+        try {
+            var salesperson = new Salesperson(salesperson_data);
+            await salesperson.save();
+            return salesperson;
+        }
+        catch(err) {
+            console.log(`Received an error creating salesperson: ${err}`);
+        }
+    },
+    getSalespeopleToEmail: async () => {
+        try {
+            let salespeople = await Salesperson.find({'get_mail': true});
+            return salespeople;
+        }
+        catch(err) {
+            console.log('Received an error getting salespeople to email');
+        }
+    },
+    updateSalesperson: async (criteria, update ) => {
+        if (criteria._id) {
+            criteria._id = mongoose.Types.ObjectId(criteria._id);
+        }
+
+        try {
+            let salesperson = await Salesperson.findOneAndUpdate(criteria, update, {new: true});
+            return salesperson;
+        }
+        catch (err) {
+            console.log('Received an error updating salesperson');
+        }
+    },
+
+    /**
+     * List all ports in the database
+     *
+     */
+    listPorts: async () => {
+        try {
+            let ports = await Port.find();
+            return ports;
+        }
+        catch (err) {
+            console.log('Received an error getting ports listing');
+        }
+    },
+
+    /**
+     * Get specific port by id
+     *
+     */
+    getPortById: async (id) => {
+        try {
+            var port_id = mongoose.Types.ObjectId(id);
+            let port = await Port.findById({_id: port_id});
+            return port;
+        }
+        catch (err) {
+            console.log('Received an error getting port');
+        }
+    },
+    /**
+     * Create port
+     *
+     */
+    createPort: async (port_data) => {
+        try {
+            var new_port = new Port(port_data);
+            await new_port.save();
+            return new_port;
+        }
+        catch(err) {
+            console.log('Received an error creating a port');
+        }
+    },
+    /**
+     * Update specific port
+     *
+     */
+    updatePort: async (criteria, update ) => {
+        if (criteria._id) {
+            criteria._id = mongoose.Types.ObjectId(criteria._id);
+        }
+
+        try {
+            let port = await Port.findOneAndUpdate(criteria, update, {new: true});
+            return port;
+        }
+        catch (err) {
+            console.log('Received an error updating port');
+        }
+    }
+}
---
+++ controllers/routingController.js
@@ -0,0 +1,288 @@
+const dataAccess        = require('./dataAccessController');
+
+module.exports = {
+    /*
+     * Customers
+     *
+     */
+    getCustomerList: async (req, res, next) => {
+        try {
+            var customers = await dataAccess.listCustomers();
+            res.status(200).json(customers);
+        }
+        catch(err) {
+            return res.status(500).json({
+                message: `Error retrieving customer list ${err}` 
+            })
+        }
+    },
+    getCustomerById: async (req, res, next) => {
+        try {
+            var customer = await dataAccess.getCustomerById(req.params.id);
+            res.status(200).json(customer);
+        }
+        catch(err) {
+            return res.status(500).json({
+                message: `Error retrieving customer ${err}` 
+            });
+        }
+    },
+    createCustomer: async (req, res, next) => {
+        try{
+            var customer = await dataAccess.createCustomer(req.body);
+            res.status(200).json(customer);
+        }
+        catch(err) {
+            return res.status(500).json({
+                message: `Error creating customer ${err}`
+            });
+        }
+    },
+    updateCustomer: async (req, res, next) => {
+        try {
+            var customer = await dataAccess.updateCustomer(req.body.criteria, req.body.update);
+            res.status(200).json(customer);
+        }
+        catch(err) {
+            return res.status(500).json({
+                message: `Error updating customer ${err}` 
+            });
+        }
+    },
+    deleteCustomer: async (req, res, next) => {
+        try{
+            var pdf_list = await dataAccess.deleteCustomer(req.body.customer);
+            req.pdf_list = pdf_list;
+            next();
+        }
+        catch(err) {
+            return res.status(500).json({
+                message: `Error deleting customer ${err}`
+            });
+        }
+    },
+    /*
+     * Quotes
+     *
+     */
+    getQuoteList: async (req, res, next) => {
+        try {
+            var quotes = await dataAccess.listQuotes();
+            res.status(200).json(quotes);
+        }
+        catch(err) {
+            return res.status(500).json({
+                message: `Error retrieving quotes list ${err}` 
+            });
+        }
+    },
+    getQuoteById: async (req, res, next) => {
+        try {
+            var quote = await dataAccess.getQuoteById(req.params.id);
+            res.status(200).json(quote);
+        }
+        catch(err) {
+            return res.status(500).json({
+                message: `Error retrieving quote ${err}` 
+            });
+        }
+    },
+    getQuotesByCustomer: async (req, res, next) => {
+        try{
+            var quotes = await dataAccess.getCustomerQuotes(req.params.cid);
+            res.status(200).json(quotes);
+        }
+        catch(err) {
+            return res.status(500).json({
+                message: `Error retrieving quote ${err}`
+            });
+        }
+    },
+    createQuote: async (req, res, next) => {
+        try{
+            // console.log(req.body.quoteObj)
+            var quote = await dataAccess.createQuote(req.body.quoteObj);
+            if (req.body.customerObj) {
+                var criteria = req.body.customerObj.criteria;
+                var data = req.body.customerObj.data;
+                var customer = await dataAccess.updateCustomer(criteria, data); 
+            }
+            res.status(200).json(quote);
+        }
+        catch(err) {
+            return res.status(500).json({
+                message: `Error creating quote ${err}`
+            });
+        }
+    },
+    updateQuote: async (req, res, next) => {
+        try{
+            var quote = await dataAccess.updateQuote(req.body.criteria, req.body.update);
+            res.status(200).json(quote);
+        }
+        catch(err) {
+            return res.status(500).json({
+                message: `Error updating quote ${err}`
+            });
+        }
+    },
+    deleteQuote: async (req, res, next) => {
+        try{
+            var pdf_list = await dataAccess.deleteQuote(req.body.quote);
+            req.pdf_list = pdf_list;
+            next();
+        }
+        catch(err) {
+            return res.status(500).json({
+                message: `Error deleting quote ${err}`
+            });
+        }
+    },
+    addQuoteDoc: async (req, res, next) => {
+        var filename = req.attachment;
+        var quote = req.body.payload;
+
+        try{
+            var quote = await dataAccess.addQuoteDoc(quote._id, filename);
+            res.status(200).json(quote);
+        }
+        catch(err) {
+            return res.status(500).json({
+                message: `Error updating quote ${err}`
+            });
+        }
+    },
+    uploadFile: async (req, res, next) => {
+        var quote_id = req.body.quote_id;
+        var filename = req.file.originalname;
+
+        try{
+            var quote = await dataAccess.addQuoteDoc(quote_id, filename);
+            res.status(200).json(quote);
+        }
+        catch(err) {
+            return res.status(500).json({
+                message: `Error updating quote ${err}`
+            });
+        }
+    },
+    /*
+     * Salespeople
+     *
+     */
+    getSalespeople: async (req, res, next) => {
+        try {
+            var salespeople = await dataAccess.getSalespeople();
+            res.status(200).json(salespeople);
+        }
+        catch(err) {
+            return res.status(500).json({
+                message: `Error retrieving salespeople list  ${err}` 
+            });
+        }
+    },
+    getSalespersonById: async (req, res, next) => {
+        try {
+            var salesperson = await dataAccess.getSalespersonById(req.params.sid);
+            res.status(200).json(salesperson);
+        }
+        catch(err) {
+            return res.status(500).json({
+                message: `Error retrieving salesperson  ${err}` 
+            });
+        }
+    },
+    createSalesperson: async (req, res, next) => {
+        try {
+            var salesperson = await dataAccess.createSalesperson(req.body);
+            res.status(200).json(salesperson);
+        }
+        catch(err) {
+            return res.status(500).json({
+                message: `Error creating salesperson  ${err}` 
+            });
+        }
+    },
+    getSalespersonByName: async (req, res, next) => {
+        try {
+            var salesperson = await dataAccess.getSalespersonByName(req.params.name);
+            res.status(200).json(salesperson);
+        }
+        catch(err) {
+            return res.status(500).json({
+                message: `Error retrieving salesperson ${err}` 
+            });
+        }
+    },
+    getSalespeopleToEmail: async (req, res, next) => {
+        try {
+            var salespeople = await dataAccess.getSalespeopleToEmail();
+            res.status(200).json(salespeople);
+        }
+        catch(err) {
+            return res.status(500).json({
+                message: `Error retrieving salesperson email recipients ${err}` 
+            });
+        }
+    },
+    updateSalesperson: async (req, res, next) => {
+        try{
+            var salesperson = await dataAccess.updateSalesperson(req.body.criteria, req.body.update);
+            res.status(200).json(salesperson);
+        }
+        catch(err) {
+            return res.status(500).json({
+                message: `Error updating salesperson ${err}`
+            });
+        }
+    },
+    /*
+     * Ports
+     *
+     */
+    getPortsList: async (req, res, next) => {
+        try {
+            var ports = await dataAccess.listPorts();
+            res.status(200).json(ports);
+        }
+        catch(err) {
+            return res.status(500).json({
+                message: `Error retrieving ports list ${err}` 
+            })
+        }
+    },
+    getPortById: async (req, res, next) => {
+        try {
+            var port = await dataAccess.getPortById(req.params.pid);
+            res.status(200).json(port);
+        }
+        catch(err) {
+            return res.status(500).json({
+                message: `Error retrieving port ${err}` 
+            });
+        }
+    },
+    createPort: async (req, res, next) => {
+        try {
+            var port = await dataAccess.createPort(req.body);
+            res.status(200).json(port);
+        }
+        catch(err) {
+            return res.status(500).json({
+                message: `Error creating port  ${err}` 
+            });
+        }
+    },
+    updatePort: async (req, res, next) => {
+        try {
+            var port = await dataAccess.updatePort(req.body.criteria, req.body.update);
+            res.status(200).json(port);
+        }
+        catch(err) {
+            return res.status(500).json({
+                message: `Error updating port ${err}` 
+            });
+        }
+    },
+
+}
