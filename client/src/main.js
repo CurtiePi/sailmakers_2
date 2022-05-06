fef71d7caf1049400db21ebe2819cbd80aff5885
CurtiePi <curtisbklyn@gmail.com>
2022-05-05 20:34:37 -0400

    added devextreme library for the dashboard graphics

--- client/src/main.js
+++ client/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue';
 import router from './router';
 import store from './store/index';
 
+import 'devextreme/dist/css/dx.light.css';
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
 
